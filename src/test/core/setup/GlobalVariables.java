0d60e641c84e7c06ef88ee0805e6e3e96aa59075
Akauê <sk2_skt@hotmail.com>
2022-03-19 11:14:54 -0300

    melhoria implementada

--- src/test/core/setup/GlobalVariables.java
+++ src/test/core/setup/GlobalVariables.java
@@ -1,2 +1,33 @@
-package core.setup;public class GlobalVariables {
+package core.setup;
+
+import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.MobileElement;
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.net.MalformedURLException;
+import java.net.URL;
+
+public class GlobalVariables {
+
+
+  public static AndroidDriver<MobileElement>  desiredcapabilities() throws MalformedURLException {
+
+    String URL = "http://localhost:4723/wd";
+    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+
+    desiredCapabilities.setCapability("platformName", "");
+    desiredCapabilities.setCapability("deviceName", "");
+    desiredCapabilities.setCapability("automationName", "");
+    desiredCapabilities.setCapability(MobileCapabilityType.APP, "");
+
+    AppiumDriver driver = new AndroidDriver(new URL(URL), desiredCapabilities);
+
+
+    return (AndroidDriver<MobileElement>) driver;
+
+  }
+
+
 }
