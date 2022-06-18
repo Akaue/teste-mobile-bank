9c91997d0fa2ab97e1ed3f1d6b4567e781b16d58
Rogerio Jordao <ro_jordao@hotmail.com>
2022-06-18 10:21:46 -0300

    Criando Pages

---
+++ src/test/financiamentos/santander/PageScreen.java
@@ -0,0 +1,22 @@
+package financiamentos.santander;
+
+import core.setup.GlobalVariables;
+import io.appium.java_client.android.AndroidDriver;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.MalformedURLException;
+
+public class teste {
+
+    public AndroidDriver driver;
+
+
+    @Test
+    public void setUp() throws MalformedURLException, InterruptedException {
+        driver = GlobalVariables.createMobile();
+    }
+
+
+
+}
